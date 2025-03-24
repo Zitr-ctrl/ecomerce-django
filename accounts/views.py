fc8d6ba8b89431479cb56e353d29339ceba43635
zitro <ajosueramos@gmail.com>
2025-03-24 13:03:33 -0500

    Lógica de los usuarios creada

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from .models import Account
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def register(request):
+    form = RegistrationForm()
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            last_name = form.cleaned_data['last_name']
+            last_name = form.cleaned_data['last_name']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+            messages.success(request, 'Se registro el usuario exitosamente')
+            return redirect('register')
+            
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Las credenciales son incorrectas')
+            return redirect('login')
+        
+    return render(request, 'accounts/login.html')
+
+@login_required(login_url = 'login')
+def logout(request):
+    
+    auth.logout(request)
+    messages.success(request, 'Has salido de sesion')
+    
+    return redirect('login')
